923c6b6844ad2699a24259888ada504e2c60b75b
Egor Troyan <troegor@qwep.ru>
2023-03-31 15:21:56 +0300

    find actual banner in query without java logic

--- src/main/java/com/troian/bannerservice/model/repo/BannerRepository.java
+++ src/main/java/com/troian/bannerservice/model/repo/BannerRepository.java
@@ -1,12 +1,40 @@
 package com.troian.bannerservice.model.repo;
 
 import com.troian.bannerservice.model.entity.Banner;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 public interface BannerRepository extends CrudRepository<Banner, Long> {
     List<Banner> findAllByIsActiveIsTrue();
 
     List<Banner> getBannersByNameContainsIgnoreCaseAndIsActiveIsTrue(String filter);
+    @Query(value = "SELECT DISTINCT b.id, b.is_active, b.name, b.price, b.text" +
+            " FROM banner b " +
+            "         JOIN category_banner cb on b.id = cb.banner_id " +
+            "         JOIN (SELECT id, is_active, name, name_id " +
+            "               FROM category\n" +
+            "               WHERE name_id IN (:categories)) c on cb.category_id = c.id " +
+            "WHERE b.id NOT IN (SELECT banner_id " +
+            "                   FROM record " +
+            "                   WHERE date(date) = CURDATE() " +
+            "                   AND ip_adress = :ip) " +
+            "ORDER BY b.price DESC LIMIT 1",
+    nativeQuery = true)
+    Optional<Banner> findBanner(@Param("categories") Set<String> categories, @Param("ip") String ip);
 }
+
+//    SELECT b.id, b.is_active, b.name, b.price, b.text
+//        FROM banner b
+//        JOIN category_banner cb on b.id = cb.banner_id
+//        JOIN (SELECT id, is_active, name, name_id
+//        FROM category
+//        WHERE name_id IN ('a', 'b')) c on cb.category_id = c.id
+//        WHERE b.id NOT IN (SELECT banner_id
+//        FROM record
+//WHERE date(date) = CURDATE())
+//        ORDER BY b.price DESC;
--- src/main/java/com/troian/bannerservice/service/BannerService.java
+++ src/main/java/com/troian/bannerservice/service/BannerService.java
@@ -4,65 +4,26 @@ import com.troian.bannerservice.aop.LoggingAspect;
 import com.troian.bannerservice.exception.NoBannerException;
 import com.troian.bannerservice.model.entity.Banner;
 import com.troian.bannerservice.model.entity.Category;
-import com.troian.bannerservice.model.entity.Record;
 import com.troian.bannerservice.model.repo.BannerRepository;
-import com.troian.bannerservice.model.repo.CategoryRepository;
-import com.troian.bannerservice.model.repo.RecordRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.util.*;
+import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Slf4j
 @Service
 public class BannerService {
     private final BannerRepository bannerRepository;
-    private final CategoryRepository categoryRepository;
-    private final RecordRepository recordRepository;
 
-    public BannerService(BannerRepository bannerRepository, CategoryRepository categoryRepository, RecordRepository recordRepository) {
+    public BannerService(BannerRepository bannerRepository) {
         this.bannerRepository = bannerRepository;
-        this.categoryRepository = categoryRepository;
-        this.recordRepository = recordRepository;
     }
     @LoggingAspect
     public Banner getBestPriceFreshBannerByCategories(Set<String> categoryIds, HttpServletRequest request) throws NoBannerException{
-        List<Banner> banners = new ArrayList<>();
-        List<Record> todayRecordsWithIp =
-                recordRepository.findRecordsByIpAndDateAfter(request.getRemoteHost(), LocalDate.now().atStartOfDay()) == null
-                        ? new ArrayList<>()
-                        : recordRepository.findRecordsByIpAndDateAfter(request.getRemoteHost(), LocalDate.now().atStartOfDay());
-
-        List<Category> categories = categoryRepository.findCategoriesByNameIdIn(categoryIds);
-        if(categories != null) {
-            categories.stream()
-                    .filter(Category::isActive)
-                    .flatMap(category -> category.getBanners().stream())
-                    .filter(banner -> todayRecordsWithIp.stream().noneMatch(r -> r.getBannerId() == banner.getId()))
-                    .forEach(banners::add);
-        }
-//        categoryIds.stream()
-//                .map(categoryRepository::getCategoryByNameId)
-//                .filter(Objects::nonNull)
-//                .filter(Category::isActive)
-//                .flatMap(category -> category.getBanners().stream())
-//                .filter(banner -> {
-//                    Record record = recordRepository.getTop1ByBannerIdAndIpOrderByDateDesc(banner.getId(), request.getRemoteHost());
-//                    return record == null || !record.getDate().toLocalDate().equals(LocalDate.now());
-//                })
-//                .forEach(banners::add);
-
-        if (banners.isEmpty()) {
-            throw new NoBannerException("No banner found");
-        }
-
-        return banners.stream()
-                .sorted()
-                .findFirst()
-                .get();
+        return bannerRepository.findBanner(categoryIds, request.getRemoteHost()).orElseThrow(() -> new NoBannerException("No banner found"));
     }
 
     public Banner addNewBanner(Banner banner, List<Category> cat) {
